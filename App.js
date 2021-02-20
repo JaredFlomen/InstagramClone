60b4ad6e939b6b616dc4ed24db03d3cc5a063294
Jared Flomen <jaredflomen@gmail.com>
2021-02-20 15:21:30 -0500

    Bug Fix

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './components/auth/Landing';
 
-const Stack = createStackNavicator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
