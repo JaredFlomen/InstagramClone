ce0a39f85191923f5fdf1a63ae11146f4d51c942
Jared Flomen <jaredflomen@gmail.com>
2021-02-20 15:18:39 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import LandingScreen from './components/auth/Landing';
 
 const Stack = createStackNavicator();
 
 export default function App() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navitor initialRouteName='Landing'>
+        <Stack.Screen
+          name='Landing'
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navitor>
+    </NavigationContainer>
+  );
 }
