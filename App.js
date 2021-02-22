244995a3913c176361447364c40b66277f153614
Jared Flomen <jaredflomen@gmail.com>
2021-02-22 11:23:37 -0500

    Add Firebase

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './components/auth/Landing';
