4b1bfeddaab8b0d1512ebc2d14866245b960b0ed
Jared Flomen <jaredflomen@gmail.com>
2021-02-22 11:28:35 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './components/auth/Landing';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
