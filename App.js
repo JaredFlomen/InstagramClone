8ff6920995d6918d191477a3682eb8ced2745a04
Jared Flomen <jaredflomen@gmail.com>
2021-02-23 09:09:18 -0500

    State for logged in

--- App.js
+++ App.js
@@ -65,17 +65,24 @@ export class App extends Component {
         </View>
       );
     }
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Landing'>
+            <Stack.Screen
+              name='Landing'
+              component={LandingScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name='Register' component={RegisterScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Landing'>
-          <Stack.Screen
-            name='Landing'
-            component={LandingScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name='Register' component={RegisterScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>User is logged in</Text>
+      </View>
     );
   }
 }
