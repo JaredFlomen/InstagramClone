ff419bd277631988adc823366700c4e364f8b65a
Jared Flomen <jaredflomen@gmail.com>
2021-02-25 09:59:11 -0500

    Setting up redux

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchUser } from '../redux/actions/index';
 
 export class Main extends Component {
   componentDidMount() {}
