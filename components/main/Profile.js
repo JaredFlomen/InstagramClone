4c4040d323ed83fa963fd0df878ede6b4413bfe4
Jared Flomen <jaredflomen@gmail.com>
2021-02-28 13:03:17 -0500

    Pulling user state in Profile

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Profile() {
+import { connect } from 'react-redux';
+
+function Profile() {
   return (
     <View>
       <Text>Profile</Text>
     </View>
   );
 }
+
+const mapStateToProps = store => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+});
+
+export default connect(mapStateToProps, null)(Profile);
