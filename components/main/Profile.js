e403b2649e921b473b56c57a36eaffa0c1b46657
Jared Flomen <jaredflomen@gmail.com>
2021-02-28 13:13:15 -0500

    Displaying User Posts

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -7,7 +7,20 @@ function Profile(props) {
   const { currentUser, posts } = props;
   return (
     <View style={styles.container}>
-      <Text>{currentUser.name}</Text>
+      <View style={styles.containerInfo}>
+        <Text>{currentUser.name}</Text>
+        <Text>{currentUser.email}</Text>
+      </View>
+      <View style={styles.containerPost}>
+        <FlatList
+          numColumns={3}
+          horizontal={false}
+          data={posts}
+          renderItem={({ item }) => (
+            <Image style={styles.image} source={{ uri: item.downloadURL }} />
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -23,5 +36,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 40,
+  },
+  containerInfo: {
+    margin: 20,
+  },
+  containerPost: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    aspectRatio: 1 / 1,
   },
 });
