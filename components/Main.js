6939143fed8cb9d1b9139331d582ae0647ad3bfe
Jared Flomen <jaredflomen@gmail.com>
2021-03-01 13:46:24 -0500

    Importing function

--- components/Main.js
+++ components/Main.js
@@ -5,7 +5,11 @@ import firebase from 'firebase';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchUser, fetchUserPosts } from '../redux/actions/index';
+import {
+  fetchUser,
+  fetchUserPosts,
+  fetchUserFollowing,
+} from '../redux/actions/index';
 
 import FeedScreen from './main/Feed';
 import ProfileScreen from './main/Profile';
@@ -21,6 +25,7 @@ export class Main extends Component {
   componentDidMount() {
     this.props.fetchUser();
     this.props.fetchUserPosts();
+    this.props.fetchUserFollowing();
   }
 
   render() {
@@ -89,6 +94,9 @@ export class Main extends Component {
 const mapStateToProps = store => ({ currentUser: store.userState.currentUser });
 
 const mapDispatchProps = dispatch =>
-  bindActionCreators({ fetchUser, fetchUserPosts }, dispatch);
+  bindActionCreators(
+    { fetchUser, fetchUserPosts, fetchUserFollowing },
+    dispatch
+  );
 
 export default connect(mapStateToProps, mapDispatchProps)(Main);
