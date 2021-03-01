def07b459eb75fe4691946bb2dcb76e2def2145c
Jared Flomen <jaredflomen@gmail.com>
2021-03-01 12:17:04 -0500

    Pull different profile on search

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import firebase from 'firebase';
+require('firebase/firestore');
 
 import { connect } from 'react-redux';
 
 function Profile(props) {
-  const { currentUser, posts } = props;
-  if (!currentUser || !posts)
-    return (
-      <View>
-        <Text>Profile Error!</Text>
-      </View>
-    );
+  const [userPosts, setUserPosts] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const { currentUser, posts } = props;
+    if (props.route.params.uid === firebase.auth().currentUser.uid) {
+      setUser(currentUser);
+      setUserPosts(posts);
+    } else {
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(props.route.params.uid)
+        .get()
+        .then(snapshot => {
+          if (snapshot.exists) {
+            setUser(snapshot.data());
+          } else {
+            console.log('Profile Error');
+          }
+        });
+      firebase
+        .firestore()
+        .collection('posts')
+        .doc(props.route.params.uid)
+        .collection('userPosts')
+        .orderBy('creation', 'asc')
+        .get()
+        .then(snapshot => {
+          let posts = snapshot.docs.map(doc => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data };
+          });
+          setUserPosts(posts);
+        });
+    }
+  }, [props.route.params.uid]);
+
+  if (!user) {
+    return <View />;
+  }
   return (
     <View style={styles.container}>
       <View style={styles.containerInfo}>
-        <Text>{currentUser.name}</Text>
-        <Text>{currentUser.email}</Text>
+        <Text>{user.name}</Text>
+        <Text>{user.email}</Text>
       </View>
       <View style={styles.containerPost}>
         <FlatList
           numColumns={3}
           horizontal={false}
-          data={posts}
+          data={userPosts}
           renderItem={({ item }) => (
             <View style={styles.containerImage}>
               <Image style={styles.image} source={{ uri: item.downloadURL }} />
@@ -43,7 +80,6 @@ export default connect(mapStateToProps, null)(Profile);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
   },
   containerInfo: {
     margin: 20,
